7291fb69bebdfd44d44c159f1cdf4781c2346ef7
Jimmy Cuadra <jimmy@jimmycuadra.com>
2016-05-11 20:42:46 -0700

    Add a Terraform output for the Kubernetes security group.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,14 @@ output "domain" {
   value = "${var.domain}"
 }
 
+output "kubernetes_security_group_id" {
+  value = "${aws_security_group.kubernetes.id}"
+}
+
 output "region" {
   value = "${var.region}"
 }
 
-output "vpc" {
+output "vpc_id" {
   value = "${aws_vpc.kubernetes.id}"
 }
