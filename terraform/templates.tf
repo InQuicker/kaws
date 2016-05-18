d3900a2375eefcc1959e23d4bce78871ad94db25
Jimmy Cuadra <jimmy@jimmycuadra.com>
2016-05-18 01:54:42 -0700

    Add create_before_destroy to template resources to remove cycle.

--- terraform/templates.tf
+++ terraform/templates.tf
@@ -1,6 +1,10 @@
 resource "template_file" "etcd_01_cloud_config" {
   template = "${file("${path.module}/templates/etcd_cloud_config.yml")}"
 
+  lifecycle {
+    create_before_destroy = true
+  }
+
   vars {
     initial_cluster_state = "${var.etcd_01_initial_cluster_state}"
     name = "etcd_01"
@@ -9,6 +13,10 @@ resource "template_file" "etcd_01_cloud_config" {
 
 resource "template_file" "etcd_02_cloud_config" {
   template = "${file("${path.module}/templates/etcd_cloud_config.yml")}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   vars {
     initial_cluster_state = "${var.etcd_02_initial_cluster_state}"
@@ -19,6 +27,10 @@ resource "template_file" "etcd_02_cloud_config" {
 resource "template_file" "etcd_03_cloud_config" {
   template = "${file("${path.module}/templates/etcd_cloud_config.yml")}"
 
+  lifecycle {
+    create_before_destroy = true
+  }
+
   vars {
     initial_cluster_state = "${var.etcd_03_initial_cluster_state}"
     name = "etcd_03"
@@ -27,6 +39,10 @@ resource "template_file" "etcd_03_cloud_config" {
 
 resource "template_file" "master_cloud_config" {
   template = "${file("${path.module}/templates/master_cloud_config.yml")}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   vars {
     cluster = "${var.cluster}"
@@ -38,6 +54,10 @@ resource "template_file" "master_cloud_config" {
 
 resource "template_file" "node_cloud_config" {
   template = "${file("${path.module}/templates/node_cloud_config.yml")}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   vars {
     cluster = "${var.cluster}"
