b6f47e0c0b41006a2be45c09e58316344fa61f7c
Jimmy Cuadra <jimmy@jimmycuadra.com>
2016-05-19 16:11:52 -0700

    Add IAM permissions for nodes to use ECR.

--- terraform/security.tf
+++ terraform/security.tf
@@ -87,6 +87,19 @@ resource "aws_iam_role_policy" "k8s_node" {
       "Effect": "Allow",
       "Action": "ec2:DetachVolume",
       "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:GetRepositoryPolicy",
+        "ecr:DescribeRepositories",
+        "ecr:ListImages",
+        "ecr:BatchGetImage"
+      ],
+      "Resource": "*"
     }
   ]
 }
