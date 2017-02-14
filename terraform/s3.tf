ee280852810e4414d83716358565784c98139238
Jimmy Cuadra <jimmy@jimmycuadra.com>
2017-02-13 18:35:23 -0800

    Add etags for S3 objects.

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -11,34 +11,40 @@ resource "aws_s3_bucket_object" "bastion_cloud_config" {
   bucket = "${aws_s3_bucket.cloud_config.id}"
   key = "bastion_cloud_config.yml"
   content = "${data.template_file.bastion_cloud_config.rendered}"
+  etag = "${md5(data.template_file.bastion_cloud_config.rendered)}"
 }
 
 resource "aws_s3_bucket_object" "etcd_01_cloud_config" {
   bucket = "${aws_s3_bucket.cloud_config.id}"
   key = "etcd_01_cloud_config.yml"
   content = "${data.template_file.etcd_01_cloud_config.rendered}"
+  etag = "${md5(data.template_file.etcd_01_cloud_config.rendered)}"
 }
 
 resource "aws_s3_bucket_object" "etcd_02_cloud_config" {
   bucket = "${aws_s3_bucket.cloud_config.id}"
   key = "etcd_02_cloud_config.yml"
   content = "${data.template_file.etcd_02_cloud_config.rendered}"
+  etag = "${md5(data.template_file.etcd_02_cloud_config.rendered)}"
 }
 
 resource "aws_s3_bucket_object" "etcd_03_cloud_config" {
   bucket = "${aws_s3_bucket.cloud_config.id}"
   key = "etcd_03_cloud_config.yml"
   content = "${data.template_file.etcd_03_cloud_config.rendered}"
+  etag = "${md5(data.template_file.etcd_03_cloud_config.rendered)}"
 }
 
 resource "aws_s3_bucket_object" "master_cloud_config" {
   bucket = "${aws_s3_bucket.cloud_config.id}"
   key = "master_cloud_config.yml"
   content = "${data.template_file.master_cloud_config.rendered}"
+  etag = "${md5(data.template_file.master_cloud_config.rendered)}"
 }
 
 resource "aws_s3_bucket_object" "node_cloud_config" {
   bucket = "${aws_s3_bucket.cloud_config.id}"
   key = "node_cloud_config.yml"
   content = "${data.template_file.node_cloud_config.rendered}"
+  etag = "${md5(data.template_file.node_cloud_config.rendered)}"
 }
