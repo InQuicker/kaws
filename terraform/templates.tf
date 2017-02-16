1682f30c58464e994c7fd4796b8e34995865a6e4
Jimmy Cuadra <jimmy@jimmycuadra.com>
2017-02-15 17:13:11 -0800

    Encode all TLS assets as Base64 to preserve formatting.

--- terraform/templates.tf
+++ terraform/templates.tf
@@ -30,11 +30,11 @@ data "template_file" "etcd_01_cloud_config" {
   template = "${file("${path.module}/templates/etcd_cloud_config.yml")}"
 
   vars {
-    etcd_ca_cert = "${file("clusters/${var.cluster}/etcd-ca.pem")}",
-    etcd_peer_ca_cert = "${file("clusters/${var.cluster}/etcd-peer-ca.pem")}",
-    etcd_peer_cert = "${file("clusters/${var.cluster}/etcd-peer.pem")}",
+    etcd_ca_cert = "${base64encode(file("clusters/${var.cluster}/etcd-ca.pem"))}",
+    etcd_peer_ca_cert = "${base64encode(file("clusters/${var.cluster}/etcd-peer-ca.pem"))}",
+    etcd_peer_cert = "${base64encode(file("clusters/${var.cluster}/etcd-peer.pem"))}",
     etcd_peer_key = "${file("clusters/${var.cluster}/etcd-peer-key-encrypted.base64")}",
-    etcd_server_cert = "${file("clusters/${var.cluster}/etcd-server.pem")}",
+    etcd_server_cert = "${base64encode(file("clusters/${var.cluster}/etcd-server.pem"))}",
     etcd_server_key = "${file("clusters/${var.cluster}/etcd-server-key-encrypted.base64")}",
     name = "etcd_01"
     region = "${var.region}"
@@ -48,11 +48,11 @@ data "template_file" "etcd_02_cloud_config" {
   template = "${file("${path.module}/templates/etcd_cloud_config.yml")}"
 
   vars {
-    etcd_ca_cert = "${file("clusters/${var.cluster}/etcd-ca.pem")}",
-    etcd_peer_ca_cert = "${file("clusters/${var.cluster}/etcd-peer-ca.pem")}",
-    etcd_peer_cert = "${file("clusters/${var.cluster}/etcd-peer.pem")}",
+    etcd_ca_cert = "${base64encode(file("clusters/${var.cluster}/etcd-ca.pem"))}",
+    etcd_peer_ca_cert = "${base64encode(file("clusters/${var.cluster}/etcd-peer-ca.pem"))}",
+    etcd_peer_cert = "${base64encode(file("clusters/${var.cluster}/etcd-peer.pem"))}",
     etcd_peer_key = "${file("clusters/${var.cluster}/etcd-peer-key-encrypted.base64")}",
-    etcd_server_cert = "${file("clusters/${var.cluster}/etcd-server.pem")}",
+    etcd_server_cert = "${base64encode(file("clusters/${var.cluster}/etcd-server.pem"))}",
     etcd_server_key = "${file("clusters/${var.cluster}/etcd-server-key-encrypted.base64")}",
     name = "etcd_02"
     region = "${var.region}"
@@ -66,11 +66,11 @@ data "template_file" "etcd_03_cloud_config" {
   template = "${file("${path.module}/templates/etcd_cloud_config.yml")}"
 
   vars {
-    etcd_ca_cert = "${file("clusters/${var.cluster}/etcd-ca.pem")}",
-    etcd_peer_ca_cert = "${file("clusters/${var.cluster}/etcd-peer-ca.pem")}",
-    etcd_peer_cert = "${file("clusters/${var.cluster}/etcd-peer.pem")}",
+    etcd_ca_cert = "${base64encode(file("clusters/${var.cluster}/etcd-ca.pem"))}",
+    etcd_peer_ca_cert = "${base64encode(file("clusters/${var.cluster}/etcd-peer-ca.pem"))}",
+    etcd_peer_cert = "${base64encode(file("clusters/${var.cluster}/etcd-peer.pem"))}",
     etcd_peer_key = "${file("clusters/${var.cluster}/etcd-peer-key-encrypted.base64")}",
-    etcd_server_cert = "${file("clusters/${var.cluster}/etcd-server.pem")}",
+    etcd_server_cert = "${base64encode(file("clusters/${var.cluster}/etcd-server.pem"))}",
     etcd_server_key = "${file("clusters/${var.cluster}/etcd-server-key-encrypted.base64")}",
     name = "etcd_03"
     region = "${var.region}"
@@ -86,11 +86,11 @@ data "template_file" "master_cloud_config" {
   vars {
     cluster = "${var.cluster}"
     domain = "${var.domain}"
-    etcd_ca_cert = "${file("clusters/${var.cluster}/etcd-ca.pem")}",
-    etcd_client_cert = "${file("clusters/${var.cluster}/etcd-client.pem")}",
+    etcd_ca_cert = "${base64encode(file("clusters/${var.cluster}/etcd-ca.pem"))}",
+    etcd_client_cert = "${base64encode(file("clusters/${var.cluster}/etcd-client.pem"))}",
     etcd_client_key = "${file("clusters/${var.cluster}/etcd-client-key-encrypted.base64")}",
-    k8s_ca_cert = "${file("clusters/${var.cluster}/k8s-ca.pem")}",
-    k8s_master_cert = "${file("clusters/${var.cluster}/k8s-master.pem")}",
+    k8s_ca_cert = "${base64encode(file("clusters/${var.cluster}/k8s-ca.pem"))}",
+    k8s_master_cert = "${base64encode(file("clusters/${var.cluster}/k8s-master.pem"))}",
     k8s_master_key = "${file("clusters/${var.cluster}/k8s-master-key-encrypted.base64")}",
     rbac_super_user = "${var.rbac_super_user}"
     region = "${var.region}"
@@ -106,11 +106,11 @@ data "template_file" "node_cloud_config" {
 
   vars {
     cluster = "${var.cluster}"
-    etcd_ca_cert = "${file("clusters/${var.cluster}/etcd-ca.pem")}",
-    etcd_client_cert = "${file("clusters/${var.cluster}/etcd-client.pem")}",
+    etcd_ca_cert = "${base64encode(file("clusters/${var.cluster}/etcd-ca.pem"))}",
+    etcd_client_cert = "${base64encode(file("clusters/${var.cluster}/etcd-client.pem"))}",
     etcd_client_key = "${file("clusters/${var.cluster}/etcd-client-key-encrypted.base64")}",
-    k8s_ca_cert = "${file("clusters/${var.cluster}/k8s-ca.pem")}",
-    k8s_node_cert = "${file("clusters/${var.cluster}/k8s-node.pem")}",
+    k8s_ca_cert = "${base64encode(file("clusters/${var.cluster}/k8s-ca.pem"))}",
+    k8s_node_cert = "${base64encode(file("clusters/${var.cluster}/k8s-node.pem"))}",
     k8s_node_key = "${file("clusters/${var.cluster}/k8s-node-key-encrypted.base64")}",
     master_ip = "kubernetes.${var.domain}"
     region = "${var.region}"
