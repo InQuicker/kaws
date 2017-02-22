780a90c3685b5ffd5deffd20ac5cf747485f7630
Jimmy Cuadra <jimmy@jimmycuadra.com>
2017-02-21 17:02:43 -0800

    Force cloud configs to depend on the generated assets by setting a variable to depend on the KMS key.

--- terraform/templates.tf
+++ terraform/templates.tf
@@ -1,3 +1,11 @@
+/*
+Note: `kms_key_id` is only included as a variable below to force a dependency on the generated
+TLS assets. Using Terraform's `depends_on` argument causes Terraform to incorrectly see the
+aws_s3_bucket_object resources as having changed, forcing each cloud config template to change, in
+turn forcing all the servers to be recreated on each `terraform apply`. It's unclear why this
+happens.
+*/
+
 data template_file "user_data" {
   template = "${file("${path.module}/templates/user_data.yml")}"
 
@@ -25,6 +33,7 @@ data "template_file" "etcd_01_cloud_config" {
     etcd_peer_key = "${file("clusters/${var.cluster}/etcd-peer-key-encrypted.base64")}",
     etcd_server_cert = "${base64encode(file("clusters/${var.cluster}/etcd-server.pem"))}",
     etcd_server_key = "${file("clusters/${var.cluster}/etcd-server-key-encrypted.base64")}",
+    kms_key_id = "${aws_kms_key.pki.key_id}"
     name = "etcd_01"
     region = "${var.region}"
     ssh_public_keys = "${join(", ", var.ssh_keys)}"
@@ -41,6 +50,7 @@ data "template_file" "etcd_02_cloud_config" {
     etcd_peer_key = "${file("clusters/${var.cluster}/etcd-peer-key-encrypted.base64")}",
     etcd_server_cert = "${base64encode(file("clusters/${var.cluster}/etcd-server.pem"))}",
     etcd_server_key = "${file("clusters/${var.cluster}/etcd-server-key-encrypted.base64")}",
+    kms_key_id = "${aws_kms_key.pki.key_id}"
     name = "etcd_02"
     region = "${var.region}"
     ssh_public_keys = "${join(", ", var.ssh_keys)}"
@@ -57,6 +67,7 @@ data "template_file" "etcd_03_cloud_config" {
     etcd_peer_key = "${file("clusters/${var.cluster}/etcd-peer-key-encrypted.base64")}",
     etcd_server_cert = "${base64encode(file("clusters/${var.cluster}/etcd-server.pem"))}",
     etcd_server_key = "${file("clusters/${var.cluster}/etcd-server-key-encrypted.base64")}",
+    kms_key_id = "${aws_kms_key.pki.key_id}"
     name = "etcd_03"
     region = "${var.region}"
     ssh_public_keys = "${join(", ", var.ssh_keys)}"
@@ -75,6 +86,7 @@ data "template_file" "master_cloud_config" {
     k8s_ca_cert = "${base64encode(file("clusters/${var.cluster}/k8s-ca.pem"))}",
     k8s_master_cert = "${base64encode(file("clusters/${var.cluster}/k8s-master.pem"))}",
     k8s_master_key = "${file("clusters/${var.cluster}/k8s-master-key-encrypted.base64")}",
+    kms_key_id = "${aws_kms_key.pki.key_id}"
     rbac_super_user = "${var.rbac_super_user}"
     region = "${var.region}"
     ssh_public_keys = "${join(", ", var.ssh_keys)}"
@@ -93,6 +105,7 @@ data "template_file" "node_cloud_config" {
     k8s_ca_cert = "${base64encode(file("clusters/${var.cluster}/k8s-ca.pem"))}",
     k8s_node_cert = "${base64encode(file("clusters/${var.cluster}/k8s-node.pem"))}",
     k8s_node_key = "${file("clusters/${var.cluster}/k8s-node-key-encrypted.base64")}",
+    kms_key_id = "${aws_kms_key.pki.key_id}"
     master_ip = "kubernetes.${var.domain}"
     region = "${var.region}"
     ssh_public_keys = "${join(", ", var.ssh_keys)}"
